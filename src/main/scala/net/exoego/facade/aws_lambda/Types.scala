abe4cb9cf526b3d0b128e66c8b373474108ca000
exoego <mogami@exoego.net>
2020-02-18 13:36:10 +0900

    Refine CloudFrontResponseEvent

--- src/main/scala/net/exoego/facade/aws_lambda/Types.scala
+++ src/main/scala/net/exoego/facade/aws_lambda/Types.scala
@@ -2589,8 +2589,30 @@ object CloudFrontResultResponse {
 }
 
 @js.native
+trait CloudFrontResponseEventRecordItem extends js.Object {
+  var config: CloudFrontEvent.Config = js.native
+  def request: CloudFrontRequest = js.native
+  var response: CloudFrontResponse = js.native
+}
+
+object CloudFrontResponseEventRecordItem {
+  def apply(
+      config: CloudFrontEvent.Config,
+      request: CloudFrontRequest,
+      response: CloudFrontResponse
+  ): CloudFrontResponseEventRecordItem = {
+    val _obj$ = js.Dynamic.literal(
+      "config" -> config.asInstanceOf[js.Any],
+      "request" -> request.asInstanceOf[js.Any],
+      "response" -> response.asInstanceOf[js.Any]
+    )
+    _obj$.asInstanceOf[CloudFrontResponseEventRecordItem]
+  }
+}
+
+@js.native
 trait CloudFrontResponseEvent extends js.Object {
-  var Records: js.Array[js.Any] = js.native
+  var Records: js.Array[CloudFrontResponseEventRecordItem] = js.native
 }
 
 object CloudFrontResponseEvent {
