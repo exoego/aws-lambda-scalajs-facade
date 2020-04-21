c839ef044e40c1cd7412653eeccb19df0037a106
exoego <mogami@exoego.net>
2020-04-21 11:53:05 +0900

    Fix typo

--- release.sh
+++ release.sh
@@ -4,5 +4,5 @@ cat ./version.sbt
 
 export SCALAJS_VERSION=0.6.32
 sbt clean +publishSigned sonatypeBundleUpload sonatypeReleaseAll
-unsert SCALAJS_VERSION
+unset SCALAJS_VERSION
 sbt clean +publishSigned sonatypeBundleUpload sonatypeReleaseAll
