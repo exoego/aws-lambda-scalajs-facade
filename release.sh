e5ab1e3806228ba54352f6f8989a06855710ff71
exoego <mogami@exoego.net>
2020-04-21 12:02:56 +0900

    Fix release command

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@
 cat ./version.sbt
 
 export SCALAJS_VERSION=0.6.32
-sbt clean +publishSigned sonatypeBundleUpload sonatypeReleaseAll
+sbt clean +publishSigned sonatypeBundleRelase
 unset SCALAJS_VERSION
-sbt clean +publishSigned sonatypeBundleUpload sonatypeReleaseAll
+sbt clean +publishSigned sonatypeBundleRelase
