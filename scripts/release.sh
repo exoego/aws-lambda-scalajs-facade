494547e87be8e90a3d1717b82e06155443a87c19
exoego <ytatsuno.jp@gmail.com>
2020-05-17 10:17:21 +0900

    Update Scala.js 0.6.x

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cat ./version.sbt
-export SCALAJS_VERSION="0.6.32"
+export SCALAJS_VERSION="0.6.33"
 sbt clean +publishSigned sonatypeBundleRelease
 unset SCALAJS_VERSION
 sbt clean +publishSigned sonatypeBundleRelease
