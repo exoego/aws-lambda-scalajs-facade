6de4b9f36f13f0b40aa7f1096ba24a3e541a2a17
exoego <ytatsuno.jp@gmail.com>
2020-08-19 21:59:30 +0900

    Add granular events for Cognito Userpool handlers
    Based on https://github.com/DefinitelyTyped/DefinitelyTyped/commit/55e2c9837e2a8330a5b155b6010efd631b67e2d2#diff-6f14f63cc2555ac17f63ed14f249707b

---
+++ src/main/scala-2.13/net/exoego/facade/aws_lambda/cognito_userpool_scala213.scala
@@ -0,0 +1,53 @@
+package net.exoego.facade.aws_lambda
+
+import net.exoego.scalajs.types.util.Factory
+
+import scala.scalajs.js
+
+/**
+  * @see https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-create-auth-challenge.html
+  */
+@Factory
+@js.native
+trait CreateAuthChallengeTriggerEvent extends BaseTriggerEvent["CreateAuthChallenge_Authentication"] {
+  var request: CreateAuthChallengeTriggerEventRequest = js.native
+  var response: CreateAuthChallengeTriggerEventResponse = js.native
+}
+
+/**
+  * @see https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-pre-authentication.html
+  */
+@Factory
+@js.native
+trait PreAuthenticationTriggerEvent extends BaseTriggerEvent["PreAuthentication_Authentication"] {
+  var request: PreAuthenticationTriggerEventRequest = js.native
+}
+
+/**
+  * @see https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-define-auth-challenge.html
+  */
+@Factory
+@js.native
+trait DefineAuthChallengeTriggerEvent extends BaseTriggerEvent["DefineAuthChallenge_Authentication"] {
+  var request: DefineAuthChallengeTriggerEventRequest = js.native
+  var response: DefineAuthChallengeTriggerEventResponse = js.native
+}
+
+/**
+  * @see https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-post-authentication.html
+  */
+@Factory
+@js.native
+trait PostAuthenticationTriggerEvent extends BaseTriggerEvent["PostAuthentication_Authentication"] {
+  var request: PostAuthenticationTriggerEventRequest = js.native
+}
+
+/**
+  * @see https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-verify-auth-challenge-response.html
+  */
+@Factory
+@js.native
+trait VerifyAuthChallengeResponseTriggerEvent extends BaseTriggerEvent["VerifyAuthChallengeResponse_Authentication"] {
+  var request: VerifyAuthChallengeResponseTriggerEventRequest = js.native
+  var response: VerifyAuthChallengeResponseTriggerEventResponse = js.native
+}
