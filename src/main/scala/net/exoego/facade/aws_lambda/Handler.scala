34c538420219bf3ffd1e6bd4f66a498729564630
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-12-24 10:32:00 +0900

    Change (Async)Handler to js.Function so it can be handler directly

--- src/main/scala/net/exoego/facade/aws_lambda/Handler.scala
+++ src/main/scala/net/exoego/facade/aws_lambda/Handler.scala
@@ -1,7 +1,6 @@
 package net.exoego.facade.aws_lambda
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
 import scala.scalajs.js.|
 
 /** Callback-style handler
@@ -9,20 +8,14 @@ import scala.scalajs.js.|
   * @tparam TEvent
   * @tparam TResult
   */
-trait Handler[TEvent, TResult] extends js.Object {
-  @JSName("apply")
-  def apply(event: TEvent, context: Context, callback: Callback[TResult]): Unit
-}
+trait Handler[TEvent, TResult] extends js.Function3[TEvent, Context, Callback[TResult], Unit] {}
 
 /** Asynchronous-style handler that returns js.Promise. This can be used Node.js 8 runtime or more
   *
   * @tparam TEvent
   * @tparam TResult
   */
-trait AsyncHandler[TEvent, TResult] extends js.Object {
-  @JSName("apply")
-  def apply(event: TEvent, context: Context): js.Promise[TResult]
-}
+trait AsyncHandler[TEvent, TResult] extends js.Function2[TEvent, Context, js.Promise[TResult]] {}
 
 @js.native
 trait Context extends js.Object {
