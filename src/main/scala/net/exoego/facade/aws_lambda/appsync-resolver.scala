743fe8a461fee84e5317de261e673a55a43e8f3b
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-12-24 09:46:12 +0900

    [appsync] Add AppSyncIdentityOIDC, AppSyncIdentityLambda, AppSyncAuthorizerEvent

--- src/main/scala/net/exoego/facade/aws_lambda/appsync-resolver.scala
+++ src/main/scala/net/exoego/facade/aws_lambda/appsync-resolver.scala
@@ -4,7 +4,7 @@ import scala.scalajs.js
 import scala.scalajs.js.|
 
 @js.native
-trait AppSyncResolverEvent[T] extends js.Object {
+trait AppSyncResolverEvent[T <: js.Any] extends js.Object {
   var arguments: T = js.native
   var identity: js.UndefOr[AppSyncIdentity] = js.native
   var source: AppSyncResolverEvent.Source | Null = js.native
@@ -104,6 +104,80 @@ object AppSyncIdentityIAM {
 }
 
 @js.native
+trait AppSyncAuthorizerEvent extends js.Object {
+  var authorizationToken: String = js.native
+  var requestContext: AppSyncAuthorizerEvent.RequestContext = js.native
+}
+
+object AppSyncAuthorizerEvent {
+  def apply(
+      authorizationToken: String,
+      requestContext: AppSyncAuthorizerEvent.RequestContext
+  ): AppSyncAuthorizerEvent = {
+    val _obj$ = js.Dynamic.literal(
+      "authorizationToken" -> authorizationToken.asInstanceOf[js.Any],
+      "requestContext" -> requestContext.asInstanceOf[js.Any]
+    )
+    _obj$.asInstanceOf[AppSyncAuthorizerEvent]
+  }
+
+  @js.native
+  trait RequestContext extends js.Object {
+    var apiId: String = js.native
+    var accountId: String = js.native
+    var requestId: String = js.native
+    var queryString: String = js.native
+    var variables: RequestContext.Variables = js.native
+  }
+
+  object RequestContext {
+    def apply(
+        apiId: String,
+        accountId: String,
+        requestId: String,
+        queryString: String,
+        variables: RequestContext.Variables
+    ): RequestContext = {
+      val _obj$ = js.Dynamic.literal(
+        "apiId" -> apiId.asInstanceOf[js.Any],
+        "accountId" -> accountId.asInstanceOf[js.Any],
+        "requestId" -> requestId.asInstanceOf[js.Any],
+        "queryString" -> queryString.asInstanceOf[js.Any],
+        "variables" -> variables.asInstanceOf[js.Any]
+      )
+      _obj$.asInstanceOf[RequestContext]
+    }
+
+    type Variables = js.Dictionary[js.Any]
+  }
+}
+
+@js.native
+trait AppSyncAuthorizerResult[TResolverContext <: js.Any] extends js.Object {
+  var isAuthorized: Boolean = js.native
+  var resolverContext: js.UndefOr[TResolverContext] = js.native
+  var deniedFields: js.UndefOr[js.Array[String]] = js.native
+  var ttlOverride: js.UndefOr[Double] = js.native
+}
+
+object AppSyncAuthorizerResult {
+  def apply[TResolverContext <: js.Any](
+      isAuthorized: Boolean,
+      resolverContext: js.UndefOr[TResolverContext] = js.undefined,
+      deniedFields: js.UndefOr[js.Array[String]] = js.undefined,
+      ttlOverride: js.UndefOr[Double] = js.undefined
+  ): AppSyncAuthorizerResult[TResolverContext] = {
+    val _obj$ = js.Dynamic.literal(
+      "isAuthorized" -> isAuthorized.asInstanceOf[js.Any]
+    )
+    resolverContext.foreach(_v => _obj$.updateDynamic("resolverContext")(_v.asInstanceOf[js.Any]))
+    deniedFields.foreach(_v => _obj$.updateDynamic("deniedFields")(_v.asInstanceOf[js.Any]))
+    ttlOverride.foreach(_v => _obj$.updateDynamic("ttlOverride")(_v.asInstanceOf[js.Any]))
+    _obj$.asInstanceOf[AppSyncAuthorizerResult[TResolverContext]]
+  }
+}
+
+@js.native
 trait AppSyncIdentityCognito extends js.Object {
   var sub: String = js.native
   var issuer: String = js.native
@@ -134,5 +208,43 @@ object AppSyncIdentityCognito {
       "groups" -> groups.asInstanceOf[js.Any]
     )
     _obj$.asInstanceOf[AppSyncIdentityCognito]
+  }
+}
+
+@js.native
+trait AppSyncIdentityOIDC extends js.Object {
+  var claims: js.Any = js.native
+  var issuer: String = js.native
+  var sub: String = js.native
+}
+
+object AppSyncIdentityOIDC {
+  def apply(
+      claims: js.Any,
+      issuer: String,
+      sub: String
+  ): AppSyncIdentityOIDC = {
+    val _obj$ = js.Dynamic.literal(
+      "claims" -> claims.asInstanceOf[js.Any],
+      "issuer" -> issuer.asInstanceOf[js.Any],
+      "sub" -> sub.asInstanceOf[js.Any]
+    )
+    _obj$.asInstanceOf[AppSyncIdentityOIDC]
+  }
+}
+
+@js.native
+trait AppSyncIdentityLambda extends js.Object {
+  var resolverContext: js.Any = js.native
+}
+
+object AppSyncIdentityLambda {
+  def apply(
+      resolverContext: js.Any
+  ): AppSyncIdentityLambda = {
+    val _obj$ = js.Dynamic.literal(
+      "resolverContext" -> resolverContext.asInstanceOf[js.Any]
+    )
+    _obj$.asInstanceOf[AppSyncIdentityLambda]
   }
 }
